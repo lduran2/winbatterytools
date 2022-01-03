7f9f533de0527ae5e49c9b45a823f5ebe24586dc
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-01-02 23:03:49 -0500

    added powershell bypass cmd script

---
+++ ps1/BatteryRange.cmd
@@ -0,0 +1,7 @@
+::::::
+:: Runs the corresponding PowerShell script.
+:: By        : Leomar Durán <https://github.com/lduran2/>
+:: When      : 2022-01-02t22:58R
+:: Version   : 1.0.0
+::::::
+PowerShell -ExecutionPolicy Bypass "./%~n0.ps1" 
